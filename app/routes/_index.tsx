084794e16513245fc0df6effdd3a769aaf937c88
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-09-05 01:04:42 +0000

    Add loader

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
+import { useLoaderData, Outlet, Link } from "@remix-run/react"
 import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare"
+import stylesUrl from "../style/index.css"
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -7,35 +10,44 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+export const links = () => {
+  return [
+    { rel: "stylesheet", href: stylesUrl },
+    { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"}
+  ];
+};
+
+type CombinedJson = {
+  myname: string;
+  githubProfile: string;
+}
+
+export const loader = async () => {
+  const combinedJson = {
+    myname: "Daisuke Yamamoto",
+    githubProfile: "https://github.com/danny-yamamoto",
+  };
+  return json(combinedJson);
+}
+
 export default function Index() {
+  const data: CombinedJson = useLoaderData();
+  console.log(data);
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      {/* Introduction Section */}
+      <section id="intro">
+        <h1>{data.myname}</h1>
+        <Outlet />
+        <p>Welcome to my portfolio</p>
+      </section>
+
+      <footer>
+        <p>&copy; 2023 {data.myname}. All rights reserved.</p>
+        <div className="social-links">
+          <Link target="_blank" to={data.githubProfile}><i className="fab fa-github"></i></Link>
+        </div>
+      </footer>
     </div>
   );
 }
